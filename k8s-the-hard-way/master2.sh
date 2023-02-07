782ce4ac93e9329aff1b8d8f582c84c280a06f24
dczinil.tython <dczinil@gmail.com>
2023-02-06 21:40:18 -0600

    configuration of the master node 2

---
+++ k8s-the-hard-way/master2.sh
@@ -0,0 +1,222 @@
+#Bootstrapping the etcd Cluster
+#The commands in this lab must be run on each controller instance: master-1,
+#and master-2. Login to each of these using an SSH terminal.
+#Bootstrapping an etcd Cluster Member
+wget -q --show-progress --https-only --timestamping \
+  "https://github.com/coreos/etcd/releases/download/v3.5.3/etcd-v3.5.3-linux-amd64.tar.gz"
+#Extract and install the etcd server and the etcdctl command line utility:
+{
+  tar -xvf etcd-v3.5.3-linux-amd64.tar.gz
+  sudo mv etcd-v3.5.3-linux-amd64/etcd* /usr/local/bin/
+}
+#Configure the etcd Server
+{
+  sudo mkdir -p /etc/etcd /var/lib/etcd /var/lib/kubernetes/pki
+  sudo cp etcd-server.key etcd-server.crt /etc/etcd/
+  sudo cp ca.crt /var/lib/kubernetes/pki/
+  sudo chown root:root /etc/etcd/*
+  sudo chmod 600 /etc/etcd/*
+  sudo chown root:root /var/lib/kubernetes/pki/*
+  sudo chmod 600 /var/lib/kubernetes/pki/*
+  sudo ln -s /var/lib/kubernetes/pki/ca.crt /etc/etcd/ca.crt
+}
+#The instance internal IP adress will be used to serve client requests,
+#and communicate with etcd cluster peers.
+INTERNAL_IP=$(ip addr show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f 1)
+MASTER_1=$(dig +short master-1)
+MASTER_2=$(dig +short master-2)
+#Each etcd member must have a unique name within an etcd cluster.
+#Set the etcd name to match the hostname of the current compute instance:
+ETCD_NAME=$(hostname -s)
+#Create the etcd.service systemd unit file:
+cat <<EOF | sudo tee /etc/systemd/system/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+ExecStart=/usr/local/bin/etcd \\
+  --name ${ETCD_NAME} \\
+  --cert-file=/etc/etcd/etcd-server.crt \\
+  --key-file=/etc/etcd/etcd-server.key \\
+  --peer-cert-file=/etc/etcd/etcd-server.crt \\
+  --peer-key-file=/etc/etcd/etcd-server.key \\
+  --trusted-ca-file=/etc/etcd/ca.crt \\
+  --peer-trusted-ca-file=/etc/etcd/ca.crt \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \\
+  --listen-peer-urls https://${INTERNAL_IP}:2380 \\
+  --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://${INTERNAL_IP}:2379 \\
+  --initial-cluster-token etcd-cluster-0 \\
+  --initial-cluster master-1=https://${MASTER_1}:2380,master-2=https://${MASTER_2}:2380 \\
+  --initial-cluster-state new \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Start the etcd Server
+{
+  sudo systemctl daemon-reload
+  sudo systemctl enable etcd
+  sudo systemctl start etcd
+}
+#Verification
+#List the etcd cluster members:
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.crt \
+  --cert=/etc/etcd/etcd-server.crt \
+  --key=/etc/etcd/etcd-server.key
+#Bootstrapping the Kubernetes Control Plane
+#Download and Install the Kubernetes Controller Binaries
+wget -q --show-progress --https-only --timestamping \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.24.3/bin/linux/amd64/kube-apiserver" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.24.3/bin/linux/amd64/kube-controller-manager" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.24.3/bin/linux/amd64/kube-scheduler" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.24.3/bin/linux/amd64/kubectl"
+#Install the Kubernetes binaries:
+{
+  chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
+  sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
+}
+#Configure the Kubernetes API Server
+{
+  sudo mkdir -p /var/lib/kubernetes/pki
+
+  # Only copy CA keys as we'll need them again for workers.
+  sudo cp ca.crt ca.key /var/lib/kubernetes/pki
+  for c in kube-apiserver service-account apiserver-kubelet-client etcd-server kube-scheduler kube-controller-manager
+  do
+    sudo mv "$c.crt" "$c.key" /var/lib/kubernetes/pki/
+  done
+  sudo chown root:root /var/lib/kubernetes/pki/*
+  sudo chmod 600 /var/lib/kubernetes/pki/*
+}
+#The instance internal IP address will be used to advertise the API Server,
+#to members of the cluster.
+INTERNAL_IP=$(ip addr show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f 1)
+LOADBALANCER=$(dig +short loadbalancer)
+#IP addresses of the two master nodes, where the etcd servers are.
+MASTER_1=$(dig +short master-1)
+MASTER_2=$(dig +short master-2)
+#CIDR ranges used within the cluster
+POD_CIDR=10.244.0.0/16
+SERVICE_CIDR=10.96.0.0/16
+#Create the kube-apiserver.service systemd unit file:
+cat <<EOF | sudo tee /etc/systemd/system/kube-apiserver.service
+[Unit]
+Description=Kubernetes API Server
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-apiserver \\
+  --advertise-address=${INTERNAL_IP} \\
+  --allow-privileged=true \\
+  --apiserver-count=2 \\
+  --audit-log-maxage=30 \\
+  --audit-log-maxbackup=3 \\
+  --audit-log-maxsize=100 \\
+  --audit-log-path=/var/log/audit.log \\
+  --authorization-mode=Node,RBAC \\
+  --bind-address=0.0.0.0 \\
+  --client-ca-file=/var/lib/kubernetes/pki/ca.crt \\
+  --enable-admission-plugins=NodeRestriction,ServiceAccount \\
+  --enable-bootstrap-token-auth=true \\
+  --etcd-cafile=/var/lib/kubernetes/pki/ca.crt \\
+  --etcd-certfile=/var/lib/kubernetes/pki/etcd-server.crt \\
+  --etcd-keyfile=/var/lib/kubernetes/pki/etcd-server.key \\
+  --etcd-servers=https://${MASTER_1}:2379,https://${MASTER_2}:2379 \\
+  --event-ttl=1h \\
+  --encryption-provider-config=/var/lib/kubernetes/encryption-config.yaml \\
+  --kubelet-certificate-authority=/var/lib/kubernetes/pki/ca.crt \\
+  --kubelet-client-certificate=/var/lib/kubernetes/pki/apiserver-kubelet-client.crt \\
+  --kubelet-client-key=/var/lib/kubernetes/pki/apiserver-kubelet-client.key \\
+  --runtime-config=api/all=true \\
+  --service-account-key-file=/var/lib/kubernetes/pki/service-account.crt \\
+  --service-account-signing-key-file=/var/lib/kubernetes/pki/service-account.key \\
+  --service-account-issuer=https://${LOADBALANCER}:6443 \\
+  --service-cluster-ip-range=${SERVICE_CIDR} \\
+  --service-node-port-range=30000-32767 \\
+  --tls-cert-file=/var/lib/kubernetes/pki/kube-apiserver.crt \\
+  --tls-private-key-file=/var/lib/kubernetes/pki/kube-apiserver.key \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Configure the Kubernetes Controller Manager
+sudo mv kube-controller-manager.kubeconfig /var/lib/kubernetes/
+#Create the kube-controller-manager.service systemd unit file:
+cat <<EOF | sudo tee /etc/systemd/system/kube-controller-manager.service
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-controller-manager \\
+  --allocate-node-cidrs=true \\
+  --authentication-kubeconfig=/var/lib/kubernetes/kube-controller-manager.kubeconfig \\
+  --authorization-kubeconfig=/var/lib/kubernetes/kube-controller-manager.kubeconfig \\
+  --bind-address=127.0.0.1 \\
+  --client-ca-file=/var/lib/kubernetes/pki/ca.crt \\
+  --cluster-cidr=${POD_CIDR} \\
+  --cluster-name=kubernetes \\
+  --cluster-signing-cert-file=/var/lib/kubernetes/pki/ca.crt \\
+  --cluster-signing-key-file=/var/lib/kubernetes/pki/ca.key \\
+  --controllers=*,bootstrapsigner,tokencleaner \\
+  --kubeconfig=/var/lib/kubernetes/kube-controller-manager.kubeconfig \\
+  --leader-elect=true \\
+  --node-cidr-mask-size=24 \\
+  --requestheader-client-ca-file=/var/lib/kubernetes/pki/ca.crt \\
+  --root-ca-file=/var/lib/kubernetes/pki/ca.crt \\
+  --service-account-private-key-file=/var/lib/kubernetes/pki/service-account.key \\
+  --service-cluster-ip-range=${SERVICE_CIDR} \\
+  --use-service-account-credentials=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Configure the Kubernetes Scheduler
+#Move the kube-scheduler kubeconfig into place:
+sudo mv kube-scheduler.kubeconfig /var/lib/kubernetes/
+#Create the kube-scheduler.service systemd unit file:
+cat <<EOF | sudo tee /etc/systemd/system/kube-scheduler.service
+[Unit]
+Description=Kubernetes Scheduler
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-scheduler \\
+  --kubeconfig=/var/lib/kubernetes/kube-scheduler.kubeconfig \\
+  --leader-elect=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Secure kubeconfigs
+sudo chmod 600 /var/lib/kubernetes/*.kubeconfig
+#Optional - Check Certificates and kubeconfigs
+echo option 3
+./cert_verify.sh
+#Start the Controller Services
+{
+  sudo systemctl daemon-reload
+  sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
+  sudo systemctl start kube-apiserver kube-controller-manager kube-scheduler
+}
+#Verification
+kubectl get componentstatuses --kubeconfig admin.kubeconfig
+
